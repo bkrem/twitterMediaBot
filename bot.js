061dd37910508bb1acf9e77c68ecc976c4def0a4
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-02-12 19:13:02 +0000

    Ensure we retry if final composed tweet is too long. Refactor function names.

--- bot.js
+++ bot.js
@@ -10,12 +10,14 @@ var T = new Twit(require('./config/twitConfig.js'));
 // Instance of Flickr API
 var flickrOpts = require('./config/flickrConfig');
 
-function getKanyePic() {
+function getPic () {
 	Flickr.tokenOnly(flickrOpts, function (error, flickr) {
 		if (error) throw new Error(error);
 
+		var textOpts = ["kanye+west", "kanye+west+kim+kardashian", "yeezus"];
+
 		flickr.photos.search({
-			text: "kanye+west"
+			text: textOpts[Math.floor( Math.random() * (textOpts.length - 1) )]
 		}, function (err, result) {
 			if (err) console.error(err);
 
@@ -54,7 +56,7 @@ function isTweetNew(tweetText, callback) {
 	var opts = { screen_name: 'YeezyNewsBot', exclude_replies: true };
 
 	T.get('statuses/user_timeline', opts, function (err, data) {
-		if (err) return console.error(err);
+		if (err) console.error(err);
 
 		// check if any previous tweet text matches the new one
 		for (var i = 0; i < data.length; i++) {
@@ -68,20 +70,27 @@ function isTweetNew(tweetText, callback) {
 }
 
 function getRandomHeadline(callback) {
-	var agency, headline;
+	var sources = ['guardian', 'NYDailyNews', 'BBCWorld', 'nytimes', 'BBCBusiness', 'BreakingNews', 'AP'];
 	var randomSelection = Math.floor(Math.random() * 10);
 	var opts = {
-		screen_name: 'guardian',
+		screen_name: sources[Math.floor(Math.random() * (sources.length-1))],
 		count: 10,
 		exclude_replies: true
 	};
+	console.log(opts.screen_name);
 
 	T.get('statuses/user_timeline', opts, function (err, data) {
-		if (err) return console.error(err);
+		if (err) console.error(err);
 
-		agency = data[randomSelection].user.name;
-		headline = data[randomSelection].text;
-		if (callback) callback(headline + " | " + agency);
+		var agency = "@" + data[randomSelection].user.screen_name;
+		var headline = data[randomSelection].text;
+		var tweet = headline + "|" + agency;
+
+		if (tweet.length > 140) {
+			console.info("Constructed tweet is too long; trying again...");
+			return main();
+		}
+		if (callback) callback(tweet);
 	});
 }
 
@@ -89,7 +98,7 @@ function mediaUpload(news) {
 	var img = util.base64_encode("yeezy.jpg");
 
 	T.post('media/upload', { media: img }, function (err, data, response) {
-		if (err) return console.error(err);
+		if (err) console.error(err);
 
 		var mediaIdStr = data.media_id_string;
 		console.log(mediaIdStr);
@@ -97,36 +106,45 @@ function mediaUpload(news) {
 		var params = { status: news, media_ids: [mediaIdStr] };
 
 		T.post('statuses/update', params, function (err, data, response) {
-			if (err) console.error(err);
+			if (err) {
+				// Probably failed due to 140 char limit => call main() to try again
+				console.error("Error at 'statuses/update': " + err);
+				return main();
+			}
 		})
 	})
 }
 
 function simpleTweet(message) {
 	T.post('statuses/update', { status: message }, function(err, data, response) {
-		if (err) return console.error(err);
+		if (err) console.error(err);
 		console.log("Tweeted " + "'" + message + "' " + "successfully: ");
 	})
 }
 
-function kanyeTweet() {
+function main() {
 	getRandomHeadline(function (news) {
 		console.log(news);
 		// Check if we've tweeted this headline before
 		isTweetNew(news, function (isNew) {
 			if (isNew) {
-				// Attach the formatted headline to the Twitter API media upload
-				mediaUpload(news);
-				// Get a new Kanye pic for next tweet
-				getKanyePic();
+				try {
+					getPic(function () {
+						// Attach the formatted headline to the Twitter API media upload
+						mediaUpload(news);
+					});
+				} catch (e) {
+					console.error(e);
+					return main();
+				}
 			} else {
 				console.log("Tweet is not new; trying again...");
-				return kanyeTweet()
+				return main()
 			}
 		})
 	});
 }
 
 // MAIN
-kanyeTweet();
-setInterval(kanyeTweet, 1000 * 60 * 5);
+main();
+setInterval(main, 1000 * 60 * 5);
