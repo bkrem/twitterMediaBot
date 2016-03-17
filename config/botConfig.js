13c8692e282286ea7166aaeebcc9bb3aa19b36c2
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-03-17 20:15:27 +0000

    Comments

--- config/botConfig.js
+++ config/botConfig.js
@@ -3,11 +3,15 @@
  */
 
 module.exports = {
+    // The bot's Twitter screen_name/username
     ACCOUNT: 'YeezyNewsBot',
     // Replace with whichever accounts you want to fetch tweets from
     SOURCES: ['guardian', 'NYDailyNews', 'BBCWorld', 'nytimes', 'BBCBusiness', 'BreakingNews', 'AP', 'ABC',
                 'nytopinion', 'washingtonpost', 'Independent', 'BBCBreaking', 'CBSNews', 'cnnbrk', 'WSJ'],
+    // Filename to save the downloaded image to
     IMG: 'img.jpg',
+    // Size of the img to be downloaded
     IMG_SIZE: 'Medium',
+    // Replace with the any keywords you want to search Flickr with
     IMG_KEYWORDS: ["kanye+west", "kanye+west+kim+kardashian", "yeezus"]
 };
